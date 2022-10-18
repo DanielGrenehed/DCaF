9c4e4495c730d193454278ccf6a441d5e142c8c1
Daniel Amos Grenehed <daniel.grenehed97@gmail.com>
2022-10-19 00:19:57 +0200

    Update dcaf.go

--- dcaf/dcaf.go
+++ dcaf/dcaf.go
@@ -7,10 +7,11 @@ import (
 	"os"
 )
 
-
-
 type Filter func(string) string
 
+/*
+	Write filtered lines 
+*/
 func process(r *bufio.Scanner, w *bufio.Writer, f Filter) {
 	for r.Scan() {
 		line := f(r.Text())
@@ -20,45 +21,40 @@ func process(r *bufio.Scanner, w *bufio.Writer, f Filter) {
 	}
 }
 
-
-
-
-func parseLine(line string, sep Segmenter, con Desegmenter) string {
-	segments := sep(line)
-	//fmt.Println(segments)
-	str := con(segments)
-	return str
+func reconstructionFilter(seg Segmenter, des Desegmenter) (Filter) {
+	return func(s string) string {return des(seg(s))}
 }
 
 
 func main() {
+	/*
+		Parse input
+	*/
 	in_file_ptr := flag.String("i", "", "file to read")
 	out_file_ptr := flag.String("o", "", "file to write")
+
 	dms_ptr := flag.String("c", "", "format of data in input")
 	djs_ptr := flag.String("r", "", "reconstruction format of line")
-	djoint_ptr := flag.String("j", ",", "default char to join data")
-	explicit_ptr := flag.Bool("e", false, "Print omitted lines")
-	flag.Parse()
 
-	if *explicit_ptr {
-		fmt.Println("Explicit output enabled")
-	}
+	djoint_ptr := flag.String("j", ",", "default char to join data")
+	
+	flag.Parse() 
 
 	/*
 		Create data flow
 	*/
 	data_matcher := constructDataMatcher(*dms_ptr)
-	seg := getDataSliceSegmenter(data_matcher)
+	
 
 	data_join_string := *djs_ptr
 	data_joiner := constructDataJoiner(data_join_string)
 
-	default_joint := []rune(*djoint_ptr)
 	if data_join_string == "" {
-		data_joiner = createMatchingDataJoiner(data_matcher, default_joint[0])
+		data_joiner = createMatchingDataJoiner(data_matcher, ([]rune(*djoint_ptr))[0])
 	}
+
+	seg := getDataSliceSegmenter(data_matcher)
 	des := getLineJointDesegmenter(data_joiner)
-	var f = func(s string) string {return parseLine(s, seg, des)}
 
 
 	/*
@@ -96,7 +92,7 @@ func main() {
 	/*
 		process data line by line
 	*/
-	process(r, w, f)
+	process(r, w, reconstructionFilter(seg, des))
 
 	/*
 		clean up
